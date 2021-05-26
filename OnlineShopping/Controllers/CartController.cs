33ca58d2839a5c8213de3a085bd68786f8daf86d
Saroj Adhikari <saroj.io@outlook.com>
2021-05-26 21:56:44 +0530

    Update quantity for same items added multiple times to cart
    Update view to display quntity

--- OnlineShopping/Controllers/CartController.cs
+++ OnlineShopping/Controllers/CartController.cs
@@ -14,18 +14,19 @@ namespace OnlineShopping.Controllers
         // GET: Cart
         public ActionResult Index()
         {
-            var cartData = Session["cart"];
-
-            var cartList = (List<Products>)cartData;
-
-            return View(cartData);
+            var cartItemsForUser = GetCartItemForUser();
+            return View(cartItemsForUser);
         }
 
         //[HttpPost]
         public ActionResult AddToCart(int id)
         {
-            var cartData = (List<Products>)Session["cart"];
+            var cartData = GetCartItemForUser();
+
             var dbProd = new Products();
+
+            var newCart = new CartViewModel();
+            newCart.user = new Users() { Email = User.Identity.Name };
 
             using(OnlineShoppingEntities dbContext = new OnlineShoppingEntities())
             {
@@ -44,18 +45,63 @@ namespace OnlineShopping.Controllers
             //}
             if (dbProd.Id == 0 && cartData == null)
             {
+
             }
             //return Json("Could not add product");
             else if (dbProd.Id != 0 && cartData != null)
-                cartData.Add(dbProd);
+            {
+                if(IsProductAlreadyOnCart(dbProd))
+                {
+                    //find the product existing on cart and increment its quantity
+                    cartData.Find(cvm => cvm.product.Id == dbProd.Id).Quantity++;
+                }
+                else
+                {
+                    newCart.product = dbProd;
+                    cartData.Add(newCart);
+                }
+                Session["cart"] = cartData;
+            }
             else if (dbProd.Id != 0 && cartData == null)
             {
-                var list = new List<Products>();
-                list.Add(dbProd);
-                Session["cart"] = list;
+                var cartList = new List<CartViewModel>();
+                newCart.product = dbProd;
+
+                if (IsProductAlreadyOnCart(dbProd))
+                {
+                    newCart.Quantity++;
+                }
+
+                cartList.Add(newCart);
+                Session["cart"] = cartList;
             }
 
             return RedirectToAction("Index");
+        }
+
+        protected List<CartViewModel> GetCartItemForUser()
+        {
+            var cartData = Session["cart"];
+
+            var allCart = (List<CartViewModel>)cartData;
+
+            var cartItemsForUser = allCart?.Where(cvm => cvm.user.Email == User.Identity.Name).ToList();
+
+            return cartItemsForUser;
+        }
+
+        protected bool IsProductAlreadyOnCart(Products product)
+        {
+            var userCart = GetCartItemForUser();
+
+            var productFromCart = userCart?.Where(cvm => cvm.product.Id == product.Id).FirstOrDefault();
+
+            if(productFromCart != null)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ OnlineShopping/Models/CartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingDataAccess;
+
+namespace OnlineShopping.Models
+{
+    public class CartViewModel
+    {
+        public Users user { get; set; }
+
+        public Products product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public CartViewModel()
+        {
+            Quantity = 1;
+        }
+    }
+}

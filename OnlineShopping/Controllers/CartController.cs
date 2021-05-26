b203cbaabd7aff809aa23498b04248932e4e5c9e
Saroj Adhikari <saroj.io@outlook.com>
2021-05-26 18:51:08 +0530

    Implemented Basic Cart based on session storage
    Fixed Navbar issue on mobile - with navbar fluid

--- OnlineShopping/Controllers/CartController.cs
+++ OnlineShopping/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineShopping.Models;
+using OnlineShoppingDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,7 +14,48 @@ namespace OnlineShopping.Controllers
         // GET: Cart
         public ActionResult Index()
         {
-            return View();
+            var cartData = Session["cart"];
+
+            var cartList = (List<Products>)cartData;
+
+            return View(cartData);
+        }
+
+        //[HttpPost]
+        public ActionResult AddToCart(int id)
+        {
+            var cartData = (List<Products>)Session["cart"];
+            var dbProd = new Products();
+
+            using(OnlineShoppingEntities dbContext = new OnlineShoppingEntities())
+            {
+                dbProd = dbContext.Products.Where(p => p.Id == id).FirstOrDefault();
+            }
+
+            //if (cartData != null && product != null)
+            //{
+            //    dbProd.Id = product.Id;
+            //    dbProd.ProductId = product.ProductId;
+            //    dbProd.ProductName = product.ProductName;
+            //    dbProd.Category = product.Category;
+            //    dbProd.Cost = product.Cost;
+            //    dbProd.Description = product.Description;
+            //    dbProd.ImageName = product.Image.FileName;
+            //}
+            if (dbProd.Id == 0 && cartData == null)
+            {
+            }
+            //return Json("Could not add product");
+            else if (dbProd.Id != 0 && cartData != null)
+                cartData.Add(dbProd);
+            else if (dbProd.Id != 0 && cartData == null)
+            {
+                var list = new List<Products>();
+                list.Add(dbProd);
+                Session["cart"] = list;
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }

c59bf8d15c223e1dc451340778c92f6328615b0c
Saroj Adhikari <saroj.io@outlook.com>
2021-05-28 15:52:30 +0530

    Lowercase rote urls and (ajax search - incomplete)

--- OnlineShoppingDB/Products.sql
+++ OnlineShoppingDB/Products.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(5000, 1), 
     [ProductId] NVARCHAR(50) NOT NULL, 
-    [ProductName] NVARCHAR(50) NOT NULL, 
+    [ProductName] NVARCHAR(200) NOT NULL, 
     [Cost] MONEY NOT NULL, 
     [Category] NVARCHAR(50) NOT NULL, 
     [ImageName] NVARCHAR(50) NULL, 
--- OnlineShoppingDataAccess/OnlineShoppingDataModel.Designer.cs
+++ OnlineShoppingDataAccess/OnlineShoppingDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Visual Studio 2019\repos\OnlineShopping\OnlineShoppingDataAccess\OnlineShoppingDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Saroj\Source\Repos\adksaroj\OnlineShopping\OnlineShoppingDataAccess\OnlineShoppingDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

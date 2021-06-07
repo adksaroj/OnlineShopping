d46d7702fe9a93b1f0bb1432921cf8ab09544d74
Saroj Adhikari <saroj.io@outlook.com>
2021-06-08 01:04:41 +0530

    N:N Mapping for Orders and Products

---
+++ OnlineShoppingDB/OrderItems.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[OrderItems]
+(
+	[OrderId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    CONSTRAINT PK_OrderItem PRIMARY KEY ([OrderId], [ProductId]),
+    CONSTRAINT FK_OrderItem_Orders FOREIGN KEY ([OrderId]) REFERENCES Orders(Id),
+    CONSTRAINT FK_OrderItem_Products FOREIGN KEY ([ProductId]) REFERENCES Products(Id),
+)

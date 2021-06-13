e7316768479c1627ea2d356e51b147736f1d6b86
Saroj Adhikari <saroj.io@outlook.com>
2021-06-14 00:08:31 +0530

    Added Console app for test and data iserts

---
+++ OnlineShoppingTestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShoppingDataAccess;
+
+namespace OnlineShoppingTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Operation...");
+
+
+
+            Console.WriteLine("Completed...");
+            Console.ReadKey();
+        }
+    }
+}
